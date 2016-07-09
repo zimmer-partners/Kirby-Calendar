1722cbf5b4564e71f7d429821804d4c90a56fb27
Moritz Zimmer <mz@ia.net>
2016-07-09 19:49:39 +0200

    Reformated event date output.

--- snippets/event-teaser.php
+++ snippets/event-teaser.php
@@ -1,16 +1,12 @@
 <article class="event">
   <h1><?= $event->summary()->escape() ?></h1>
-  <?= $event->description()->kirbytext() ?>
+  <div class="description">
+    <?= $event->description()->kirbytext() ?>
+  </div>
   <div class="calendar-entry">
     <?php $debug = $event->begin_timestamp()->value ?>
-    <ul>
-      <li class="begin">
-        <?= $event->begin_date_time() ?>
-      </li>
-      <li class="end">
-         <?= $event->end_date_time() ?>
-      </li>
-    </ul>
-    
+    <p class="date">
+      <span class="begin"><?= $event->begin_date_time() ?></span> – <span class="end"><?= $event->end_date_time() ?></span>
+    </p>
   </div>
 </article>
