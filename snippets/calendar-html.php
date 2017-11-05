26ccfb110d176a995fba32d6725f531444bef7c5
moritzz <mz@ia.net>
2017-11-05 15:17:29 +0100

    Updated HTML output to match latest Starterkit.

--- snippets/calendar-html.php
+++ snippets/calendar-html.php
@@ -4,28 +4,32 @@
   
   <main class="main" role="main">
 
-    <div class="text">
-      <h1><?= $page->title()->html() ?></h1>
-      <?= $page->text()->kirbytext() ?>
-    </div>
+    <header class="wrap">
+	    <div class="text">
+	      <h1><?= $page->title()->html() ?></h1>
+	      <?= $page->text()->kirbytext() ?>
+	    </div>
+	</header>
     
-    <?php if ($page->webcal_text()->isNotEmpty()): ?>
-      <section class="webcal">
-        <p>
-          <a class="webcal-link" href="<?= $page->webcal_url() ?>"><img class="webcal-icon" src="<?= $site->url() ?>/assets/plugins/calendar/images/ics-file-format-symbol.svg" alt="ICS File" height="32px" border="0"/><?= $page->webcal_text()->escape() ?></a>
-        </p>
-      </section>
-    <?php endif ?>
-    
-    <?php $events = $page->events($own = true, $allies = array('children' => true, 'siblings' => true)); ?>
-    <?php $debug = $events->count() ?>
-    <?php if ($events->count() > 0): ?>
-      <section class="events">
-        <?php foreach ($events as $key => $event): ?>
-          <?php snippet('event-teaser', array('event' => $event)); ?>
-        <?php endforeach ?>
-      </section>
-    <?php endif ?>
+    <div class="text wrap">
+	    <?php if ($page->webcal_text()->isNotEmpty()): ?>
+	      <section class="webcal">
+	        <p>
+	          <a class="webcal-link" href="<?= $page->webcal_url() ?>"><img class="webcal-icon" src="<?= $site->url() ?>/assets/plugins/calendar/images/ics-file-format-symbol.svg" alt="ICS File" border="0"/><?= $page->webcal_text()->escape() ?></a>
+	        </p>
+	      </section>
+	    <?php endif ?>
+	    
+	    <?php $events = $page->events($own = true, $allies = array('children' => true, 'siblings' => true)); ?>
+	    <?php $debug = $events->count() ?>
+	    <?php if ($events->count() > 0): ?>
+	      <section class="events">
+	        <?php foreach ($events as $key => $event): ?>
+	          <?php snippet('event-teaser', array('event' => $event)); ?>
+	        <?php endforeach ?>
+	      </section>
+	    <?php endif ?>
+	</div>
     
   </main>
 
